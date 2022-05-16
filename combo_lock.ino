15d7c2c79b449ea702ad36bb81c174783d013b10
Jaeheon Shim <git@jaeheonshim.com>
2022-05-16 17:16:49 -0400

    Add distance sensing

--- combo_lock.ino
+++ combo_lock.ino
@@ -2,6 +2,8 @@
 #include <Servo.h>
 
 #define SERVO_PIN 3
+#define ECHO_PIN 4
+#define TRIG_PIN 5
 
 #define COMBO_1 22
 #define COMBO_2 24
@@ -22,7 +24,12 @@ void setup() {
   servo.write(50);
 
   myStepper.setSpeed(120);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
   Serial.begin(9600);
+  Serial.println("Welcome. Speed is set at 120 rpm");
 }
 
 void loop() {
@@ -33,24 +40,51 @@ void loop() {
     command.trim();
 
     if (command == "REVS") {
-      Serial.println("Stepping " + String(data) + " revolutions");
+      Serial.print("Stepping " + String(data) + " revolutions.");
       stepRevs(data);
     } else if (command == "CCW") {
-      Serial.println("Turning to " + String(data) + " counterclockwise");
+      Serial.print("Turning to " + String(data) + " counterclockwise.");
       dialCombo(data, true);
     } else if (command == "CW") {
-      Serial.println("Turning to " + String(data) + " clockwise");
+      Serial.print("Turning to " + String(data) + " clockwise.");
       dialCombo(data, false);
     } else if (command == "SERVO") {
+      Serial.print("Running servo.");
       runServo();
     } else if (command == "COMBO") {
+      Serial.print("Dialing combination.");
       dialFullCombination();
     } else if(command == "FULL") {
+      Serial.print("Dialing combination and running servo!");
       dialFullCombination();
-      delay(200);
       runServo();
+    } else if(command == "SPEED") {
+      Serial.print("Setting speed to " + String(data) + " rpm.");
+      myStepper.setSpeed(data);
+    } else if(command == "SENSE") {
+      while(!Serial.available()) {
+        getDistance();
+      }
+    } else {
+      Serial.print("Invalid command.");
     }
+
+    Serial.println(" Done.");
   }
+}
+
+void getDistance() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  int distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
 }
 
 void runServo() {
